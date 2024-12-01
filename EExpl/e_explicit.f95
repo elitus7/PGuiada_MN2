0b29cd6d7e7fab6733c4021a2a37a8e8ed6da181
elitus7 <ericgb72004@gmail.com>
2024-12-01 11:55:33 +0100

    Mètode Euler Explícit

--- EExpl/e_explicit.f95
+++ EExpl/e_explicit.f95
@@ -2,17 +2,17 @@
 program e_explicit
     implicit none(type, external)
 
-    real, parameter :: cv = 3686, p = 1081, k = 0.56, s = 0.472, pext = 944000 !Constant físiques necessàries.
+    real, parameter :: cv = 3686, p = 1081, k = 0.56, s = 0.472, pext = 944000, Veff = 40 / ((2)**(0.5)) !Constant físiques necessàries.
     real, parameter :: znorm = 0.02 !Factor de normalització espacial.
     real :: tnorm = ((znorm**2)*cv*p)/k !Factor de normalització temporal.
-    real :: T_norm = 36.5 + 273.15 !Factor de normalització de la temperatura en graus Kelvin.
+    real :: T_norm =  ((Veff**2) * s)/k !Factor de normalització de la temperatura en graus Kelvin.
     integer :: n, j !Índexs que usarem per iterar. n serà l'índex temporal i j serà l'índex espacial.
     integer, parameter :: Nz = 101 !Nombre de punts del mallat espacial.
     integer, parameter :: Ntmax = 30000 !Nombre de punts màxim del mallat espacial (em feia falta definir-ho perquè per treballar amb matrius les dimensions han de ser "paramter"...).
     integer :: Nt !Nombre de punts del mallat temporal.
     real, parameter :: z0 = 0, zf = 1 !Interval espacial normalitzat.
     real, parameter :: t0 = 0, tf = 0.025 !Interval temporal normalitzat.
-    real, parameter :: T_0 = 1 !Condició inicial i de contorn de temperatura.
+    real, parameter :: T_0 = (36.5+273.15) !Condició inicial i de contorn de temperatura.
     real :: gamma !Relació entre les amplades del mallat espacial i el mallat temporal.
     real :: T(nz), T_new(nz) !Vectors de temperatura que usarem en el bucle.
     real :: T_results(Nz, Ntmax) !Array que usem per guardar tot el conjunt de resultats. Caldrà assegurar-nos de què Nt no excedeix Ntmax!
@@ -20,6 +20,7 @@ program e_explicit
     real :: t_real(Ntmax)
     real :: z_real(Nz)
     write(*,*) tnorm
+    write(*,*) T_norm
     !Cálcul del nombre de punts del mallat temporal. Usant la subrutina calculNt.
     gamma = 0.25
     call calculNt(tf, gamma, Nt)
@@ -35,14 +36,14 @@ program e_explicit
         stop
     end if
 
-    !Inicialitzem els valors de temperatura. La temperatura inicial és en tots els punts T0.
-    T = T_0
-    T_new = T_0
+    !Inicialitzem els valors de temperatura i els normalitzem amb el factor de normalització per a la temperatura. La temperatura inicial és en tots els punts T0.
+    T = T_0/T_norm
+    T_new = T_0/T_norm
 
     T_results(:,1) = T !Guardem aquesta informació. La primera columna es correspondrà amb els valors de T en tots els punts de l'espai per a temps inicial.
     !Primer apliquem les condicions de contorn, de forma que la temperatura al primer punt i al darrer siguin T0.
-    T_new(1) = T_0 !Per z inicial.
-    T_new(Nz) = T_0 !Per z final.
+    T_new(1) = T_0/T_norm !Per z inicial.
+    T_new(Nz) = T_0/T_norm !Per z final.
 
     !Bucle que ens donarà la matriu resultat, és a dir, el vector de temperatures.
     do n = 1, Nt
@@ -69,7 +70,7 @@ end do
 open(unit=10, file="eulex_results.dat", status="replace")
     do n = 1, Nt
         t_real(n) = (n-1)*Deltat*tnorm
-        write(10, *) (T_results(j, n)*T_norm - 273.15, j = 1, Nz) !A l'arxiu, la primera columna és el temps real. Cada columna fa referència a una posició i cada fila a un instant de temps.
+        write(10, *) (T_results(j, n)*T_norm - 273.15, j = 1, Nz) !Cada columna fa referència a una posició i cada fila a un instant de temps.
     end do
 close(10)
 write(*,*) "========================="
